ff2ab856aa1440a88217559274e1515867c75cee
github username <github email address>
2020-01-21 16:37:24 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+    attr_accessor :name, :artist
+
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def self.all 
+        @@all
+    end
+
+    def self.new_by_filename(filename)
+        artist_name = filename.split('-')[0].strip
+        song = Song.new(filename.split('-')[1].strip)
+        song.artist = Artist.find_or_create_by_name(artist_name)
+        song
+    end
+
+    def artist_name=(artist_name)
+        self.artist = Artist.find_or_create_by_name(artist_name)
+    end
+
+
+end
